5278cb62f9b7523bf963148fee80f980ce456848
Akbar Suriaganda <akbar.suriaganda@gmail.com>
2022-05-22 17:45:58 +0200

    Week_04 done

--- VRHF_2022_Project/Assets/Scripts/CoinCollect.cs
+++ VRHF_2022_Project/Assets/Scripts/CoinCollect.cs
@@ -1,27 +1,40 @@
-using System;
 using UnityEngine;
 
 public class CoinCollect : MonoBehaviour
 {
+    // storing the hash of the trigger beforehand gives a bit of performance (good practice)
     private static readonly int Collect = Animator.StringToHash("Collect");
 
     private Animator _animator;
+    private AudioSource _audio;
+    
     private Transform _player;
     private Vector3 _playerStartPos;
 
     private void Start()
     {
+        // get object bounded components
         _animator = GetComponent<Animator>();
+        _audio = GetComponent<AudioSource>();
+        
+        // get the player from the scene and store it's start position for later assignment
         _player = GameObject.FindWithTag("Player").transform;
         _playerStartPos = _player.position;
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        // do nothing if the other object is not player
         if (!other.CompareTag("Player")) return;
+        
+        // otherwise trigger the collect animation and play the sound
         _animator.SetTrigger(Collect);
+        _audio.Play();
     }
-
+    
+    /// <summary>
+    /// This function is called from the collect animation after it's done.
+    /// </summary>
     public void ResetPlayer()
     {
         _player.position = _playerStartPos;
